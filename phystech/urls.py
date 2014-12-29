96cd59a85619f4fe479c3872c1fe7506e76c8087
Nikolay Zinov <nzinov@gmail.com>
2014-12-29 11:50:24 +0300

    add application stub

--- phystech/urls.py
+++ phystech/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'phystech.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('main.urls')),
 )
