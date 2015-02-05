f36b023a2d0feb7eda28945741549b8229c16f1c
Nikolay Zinov <nzinov@gmail.com>
2015-02-05 16:47:33 +0300

    refactor catalog structure

--- phystech/urls.py
+++ phystech/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from django.contrib.auth.views import login
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('main.urls')),
-)
+                       url(r'^$', views.index, name='index'),
+                       url(r'^accounts/login$', login, name='login'),
+                       url(r'^game$', views.game, name='game')
+                       url(r'^admin/', include(admin.site.urls)),
+                      )
