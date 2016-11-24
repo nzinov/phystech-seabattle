0899ae22c353aa3623f72759b985f5d0ea53cd2a
Nikolay Zinov <nzinov@gmail.com>
2016-11-24 19:03:09 +0300

    add rules

---
+++ model/rules.py
@@ -0,0 +1,8 @@
+class GameRules:
+    FIELD_SIZE = 14
+    DISPLACE_SIZE = 5
+    FIG_COUNT = {"Av": 1, "Lk": 2, "Kr": 6, "Rd": 2, "Es": 6,
+            "KrPl": 1, "St": 7, "Tk": 6, "Tr": 7, "Pl": 4,
+            "Sm": 1, "Rk": 1, "T": 4, "Mn": 7, "Br": 2,
+            "AB": 1, "NB": 1, "F": 2, "Tp": 1}
+
