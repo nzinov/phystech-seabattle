28b3e3df669bbfcd23f4fb0b7d7c617fae49f83a
Nikolay Zinov <nzinov@gmail.com>
2022-08-31 16:26:02 +0300

    add token to params

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import Cookies from 'universal-cookie';
 import {v4 as uuid} from "uuid";
 
 const cookies = new Cookies();
-if (!cookies.get('token')) {
-    cookies.set('token', uuid(), { path: '/' });
-}
-
 const search = window.location.search;
 let params = new URLSearchParams(search);
+
+if (!cookies.get('token')) {
+    let token = params.get('token');
+    if (!token) {
+        token = uuid();
+    }
+    cookies.set('token', token, { path: '/' });
+}
+
 let matchID = params.get('match');
 let playerID = params.get('player');
 if (!matchID) {
@@ -23,7 +28,12 @@ if (!matchID) {
     let other = new URL(window.location);
     params.set('player', 1);
     other.search = params.toString();
+    params.set('player', 0);
     prompt("Link to join (copy to clipboard): ", other.toString());
+}
+if (!params.get('token')) {
+    params.set('token', cookies.get('token'));
+    window.location.search = params.toString();
 }
 console.log(matchID, playerID);
 
